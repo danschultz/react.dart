b0c353c29c6a45377ca2997b71bd03329d136a38
Dan Schultz <schultz.t.dan@gmail.com>
2015-10-20 18:24:17 -0700

    Add a lengthy comment on how Dart and JS components interact

--- lib/react/factory.dart
+++ lib/react/factory.dart
@@ -3,6 +3,20 @@ part of react;
 typedef ReactElement ComponentFactory([Map props, children]);
 
 ComponentFactory registerComponent(Component factory()) {
+  // When a component is registered, an internal ClassSpecification is created which
+  // contain the component lifecycle callbacks. These callbacks are invoked in JS,
+  // then forwarded to their Dart components. A ReactClass and component factory is then
+  // created for each registered component using JS interop.
+  //
+  // The mapping between the JS React component and the Dart React component are stored
+  // in `components`. When a lifecycle callback is invoked by JS, a lookup is performed
+  // and the equivalent lifecycle callback is invoked on the Dart component.
+  //
+  // On each render, the new props for the Dart component are temporarily stored in
+  // `componentProps`. A copy of the props is created, and a unique key is generated
+  // based on the props' hash code. The key is assigned as a prop on the internal React
+  // component, which then can be used as a lookup to retrieve the props in the lifecycle
+  // callbacks.
   var components = <internal.Component, Component>{};
   var componentProps = <int, Map>{};
 
@@ -28,9 +42,9 @@ ComponentFactory registerComponent(Component factory()) {
           return false;
         }
       }),
-      componentWillUpdate: allowInteropCaptureThis((jsComponent, jsNextArgs, jsNextState, jsPrevContext) {
+      componentWillUpdate: allowInteropCaptureThis((jsComponent, jsNextProps, jsNextState, jsPrevContext) {
         var component = components[jsComponent];
-        var nextProps = componentProps[jsNextArgs["propKey__"]];
+        var nextProps = componentProps[jsNextProps["propKey__"]];
 
         component.componentWillUpdate(nextProps, component._nextState);
         component._props = nextProps;
